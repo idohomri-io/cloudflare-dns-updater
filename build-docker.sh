a7d441d211ed5ca272e57099f13690c722106e5f
idohomri-io <ido@idohomri.io>
2026-02-25 23:57:44 +0200

    Delete build-docker.sh

--- build-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-docker build --platform linux/amd64 -t cfddns "$SCRIPT_DIR" --no-cache
-docker tag cfddns ghcr.io/idohomri-io/cfddns:latest
-docker push ghcr.io/idohomri-io/cfddns:latest
